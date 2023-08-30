b98a60dd6de6a35831b55f03f5dbcd18c2820f6d
Tomás <tomasleggire@hotmail.com>
2023-08-29 21:32:32 -0300

    Se agrega el toString a rectangulo

--- src/ar/edu/unlam/pb2/figuras/Rectangulo.java
+++ src/ar/edu/unlam/pb2/figuras/Rectangulo.java
@@ -9,5 +9,11 @@ public class Rectangulo {
 		this.base = base;
 		this.altura = altura;
 	}
+
+	@Override
+	public String toString() {
+		return "Rectangulo [base=" + base + ", altura=" + altura + "]";
+	}
+	
 	
 }
